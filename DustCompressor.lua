21b177429aff4d0da5ae2532bba2a1dbcddeba56
wedro22 <wedro2208@gmail.com>
2021-11-20 09:37:29 +0300

    adding dusts and nuggets

--- DustCompressor.lua
+++ DustCompressor.lua
@@ -1,4 +1,4 @@
-﻿local all_period = 20
+﻿local all_period = 60
 local device_all = {}  --список всех устройств сети, обновляется refrechDevices()
 local storagename = "storagedrawers:controller"  --хранилка пылей
 local storage  --таблица хранилка пылей
@@ -12,7 +12,10 @@ local chest_names = {
 	largishchest = "xu2:tilelargishchest"
 }
 --стандартный шаблон грегтех итемов от информации getItemDetail = пыль? меньше чем damage и соотв. имя
-local gID = {damage = 1000, name = "gregtech:meta_item_1"}
+local gID = {dustTiny_min_damage = 0, dustTiny_max_damage = 999, name = "gregtech:meta_item_1",
+			 dust_min_damage = 2000, dust_max_damage = 2999,
+			 dustSmall_min_damage = 1000, dustSmall_max_damage = 1999}
+local minecraft = {"minecraft:iron_nugget", "minecraft:gold_nugget")
 
 
 
@@ -70,11 +73,20 @@ local function unloadAll()
 	end
 end
 
+--для крафта 3х3
 local function getTurtleSlot(index)
 	slot = index
 	if index>=4 and index<=6 then
 		slot = index+1
 	elseif index>6 then
+		slot = index+2
+	end
+	return slot
+end
+--для крафта 2х2
+local function getTurtleSlotSmall(index)
+	slot = index
+	if index>2 then
 		slot = index+2
 	end
 	return slot
@@ -90,30 +102,60 @@ local function chest_stack(chest)
 	if (not lut) or #lut == 0 then
 		return
 	end
-	--составление таблицы всех пылей сортируя по параметру damage типа
+	--составление таблицы всех tiny пылей сортируя по параметру damage типа
 	--таблицаПыли{пыль1(в качестве имени - damage)={номер слота=кол-во, номер слота=кол-во}, пыль2={}}
-	dusts = {}  --таблица всех этих пылей
-	dustsSumm = {} --некрасиво, для порядка первой таблицы, подсчет сумм пылей идёт в отдельной типа имя = сумма
+	dustsTiny = {}  --таблица всех tiny пылей
+	dustsTinySumm = {}  --некрасиво, для порядка первой таблицы, подсчет сумм пылей идёт в отдельной типа имя = сумма
+	dustsSmall = {}
+	dustsSmallSumm = {}
+	
+	dusts = {}  --простые пыли. хранит ТОЛЬКО номера слотов, простой массив
+	items = {}  --другие итемы напр. минекрафт наггеты
+	
 	for i, item in pairs(lut) do
 		--gregtech "gregtech:meta_item_1"
 		if item.name == gID.name then
+		
 			--если dustTiny
-			if item.damage < gID.damage then
+			
+			if (item.damage >= gID.dustTiny_min_damage) and (item.damage <= gID.dustTiny_max_damage) then
 				--если поля пыли еще нет
-				if not dusts[item.damage] then
-					dusts[item.damage] = {}
-					dustsSumm[item.damage] = 0
+				if not dustsTiny[item.damage] then
+					dustsTiny[item.damage] = {}
+					dustsTinySumm[item.damage] = 0
 				end
-				dusts[item.damage][i] = item.count
-				dustsSumm[item.damage] = dustsSumm[item.damage] + item.count
+				dustsTiny[item.damage][i] = item.count
+				dustsTinySumm[item.damage] = dustsTinySumm[item.damage] + item.count
+				
+			--если dust
+			
+			elseif (item.damage >= gID.dust_min_damage) and (item.damage <= gID.dust_max_damage) then
+				table.insert(dusts, i)
+				
+			--если smallDust
+			
+			elseif (item.damage >= gID.dustSmall_min_damage) and (item.damage <= gID.dustSmall_max_damage) then
+				--если поля пыли еще нет
+				if not dustsSmall[item.damage] then
+					dustsSmall[item.damage] = {}
+					dustsSmallSumm[item.damage] = 0
+				end
+				dustsSmall[item.damage][i] = item.count
+				dustsSmallSumm[item.damage] = dustsSmallSumm[item.damage] + item.count
+				
+			--если 
 			end
+			
+		--minecraft
+		elseif (item.name == minecraft[1]) or (item.name == minecraft[2]) then
+			table.insert(items, i)
 		end
 	end
 	
-	
-	for dust, count in pairs(dustsSumm) do
+	-- работа с tiny dust
+	for dust, count in pairs(dustsTinySumm) do
 		--math.floor(count/9)
-		if count>9 then
+		if count>=9 then
 			maxcount = count
 			if maxcount > 576 then
 				maxcount = 576
@@ -124,7 +166,7 @@ local function chest_stack(chest)
 			
 			index = 1
 			
-			for cell, cellcount in pairs(dusts[dust]) do
+			for cell, cellcount in pairs(dustsTiny[dust]) do
 				lim = n9-turtle.getItemCount(getTurtleSlot(index))
 				if lim>0 then
 					turtle_get(chest, cell, lim, getTurtleSlot(index))
@@ -144,7 +186,51 @@ local function chest_stack(chest)
 		end
 	end
 	
+	--работа с dust простыми
+	for _, cell in pairs(dusts) do
+		turtle_get(chest, cell, 64, 16)
+		unloadAll()
+	end
 	
+	--работа с Small dust
+	for dust, count in pairs(dustsSmallSumm) do
+		--math.floor(count/4)
+		if count>=4 then
+			maxcount = count
+			if maxcount > 256 then
+				maxcount = 256
+			end
+			
+			n4 = math.floor(maxcount/4)
+			
+			
+			index = 1
+			
+			for cell, cellcount in pairs(dustsSmall[dust]) do
+				lim = n4-turtle.getItemCount(getTurtleSlotSmall(index))
+				if lim>0 then
+					turtle_get(chest, cell, lim, getTurtleSlotSmall(index))
+				end
+				while ((turtle.getItemCount(getTurtleSlotSmall(index))>=n4) and (index<4)) do
+					index = index + 1
+					lim = n4-turtle.getItemCount(getTurtleSlotSmall(index))
+					if lim>0 then
+						turtle_get(chest, cell, lim, getTurtleSlotSmall(index))
+					end
+				end
+			end
+			
+			turtle.select(16)
+			turtle.craft(64)
+			unloadAll()
+		end
+	end
+	
+	--работа с items простыми
+	for _, cell in pairs(items) do
+		turtle_get(chest, cell, 64, 16)
+		unloadAll()
+	end
 end
 
 

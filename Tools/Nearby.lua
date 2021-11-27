17b4dd52c1d12d692def56bc3dc76425e5574b4d
wedro22 <wedro2208@gmail.com>
2021-11-27 13:37:54 +0300

    Модуль Nearby

---
+++ Tools/Nearby.lua
@@ -0,0 +1,44 @@
+Nearby = {}
+
+
+---getTypes Defines the periphery around itself
+---@return table as {side = type}
+function Nearby.getTypes()
+    list = {}
+        list.left = peripheral.getType("left")
+        list.right = peripheral.getType("right")
+        list.top = peripheral.getType("top")
+        list.bottom = peripheral.getType("bottom")
+        list.front = peripheral.getType("front")
+        list.back = peripheral.getType("back")
+    return list
+end
+
+---getSimilarFromTable Compares peripherals to a list
+---@param list table list from which to choose
+---@param name string what to look for by prefix
+---@param name table what to look for by prefix
+---@return table as {side = type}
+function Nearby.getSimilarFromTable(list, name)
+    local names = {}
+    if type(name) == "string" then
+        for side, n in pairs(list) do
+            if string.sub(n, 1, #name) == name then
+                names[side] = n
+            end
+        end
+    elseif type(name) == "table" then
+        for side, n in pairs(list) do
+            for _, t in pairs(name) do
+                if string.sub(n, 1, #t) == t then
+                    names[side] = n
+                end
+            end
+        end
+    end
+    return names
+end
+
+
+
+return Nearby
---
+++ Tools/NearbyTest.lua
@@ -0,0 +1,16 @@
+local filename = "Nearby"
+----------------------------------
+local path = fs.getDir(shell.getRunningProgram())
+local file = path.."/"..filename..".lua"
+print(file)
+assert(fs.exists(file))
+r = require(filename)
+----------------------------------
+names = r.getTypes()
+for s, n in pairs(names) do
+    print(s, n)
+end
+names = r.getSimilarFromTable(r.getNamesRemote(), {"minecraft:"})
+for s, n in pairs(names) do
+    print(s, n)
+end

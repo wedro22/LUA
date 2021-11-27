51ae3efdf2319b1748d52441dd558a7772821875
wedro22 <wedro2208@gmail.com>
2021-11-27 11:50:56 +0300

    разделение на модули getNamesRemote debug

--- Tools/Net.lua
+++ Tools/Net.lua
@@ -7,7 +7,8 @@ function Net.getNamesRemote()
     --s, e = pcall(openModem)
     --if not pcall(openModem) then msg("modem error"); end
     s, e = pcall(function()
-        modem = peripheral.find("modem").open(15)
+        modem = peripheral.find("modem"); sleep(0.5)
+        modem.open(15); sleep(0.5)
         names = modem.getNamesRemote()
     end)
     if not s then print("warning: modem not found"); end
